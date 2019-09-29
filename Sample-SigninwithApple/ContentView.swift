4a909a835f57f2758e4e24d863c0edb379047911
takumi314 <isospin_symmetry@me.com>
2019-09-29 16:37:07 +0900

    implement new private method for Handling ButtonTaps

--- Sample-SigninwithApple/ContentView.swift
+++ Sample-SigninwithApple/ContentView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct ContentView: View {
     var body: some View {
@@ -24,11 +25,27 @@ struct ContentView: View {
                 // Add Sign in with Apple UI View here
                 SignInWithApple()
                     .frame(width: 280, height: 60)
+                    .onTapGesture(perform: showAppleLogin)
             }
 
         }
         .onAppear()
     }
+}
+
+extension ContentView {
+
+    private func showAppleLogin() {
+        // 1. All sign in requests need an ASAuthorizationAppleIDRequest.
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+
+        // 2. Specify the type of end user data you need to know.
+        request.requestedScopes = [.fullName, .email]
+
+        // 3. Generate the controller which will display the sign in dialog.
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+    }
+
 }
 
 #if DEBUG
